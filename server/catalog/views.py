92f63976dcc3a42d190dcfe9bb33c431acb8a2ec
Darlington Onwuama <darlingtonsomto@gmail.com>
2021-08-25 22:16:20 +0200

    added serializer for the models

--- server/catalog/views.py
+++ server/catalog/views.py
@@ -1,11 +1,15 @@
+from django.db import models
 from django.shortcuts import render
 from django.http import JsonResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .models import  *
+from .serializer import *
 
 @api_view(['GET'])
 def rooms_view(request):
-  return Response({'Rooms': 'yes'})
+  rooms = Room.objects.all()
+  return Response(RoomSerializer(rooms, many=True).data)
 
 @api_view(['GET'])
 def room_detail_view(request, pk):
